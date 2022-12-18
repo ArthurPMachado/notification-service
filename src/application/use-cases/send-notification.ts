02efd317d58c4545fcb08597ccd25d9c6bd3a3b5
Arthur Machado <arthur.machado@iclubs.tech>
2022-12-18 13:51:06 -0300

    feat: create notification repository

--- src/application/use-cases/send-notification.ts
+++ src/application/use-cases/send-notification.ts
@@ -1,11 +1,14 @@
 import { Content } from '../entities/content';
 import { Notification } from '../entities/Notification';
+import { NotificationsRepository } from '../repositories/notifications-repository';
 import {
   ISendNotificationRequest,
   ISendNotificationResponse,
 } from './interfaces/ISendNotification';
 
 export class SendNotification {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
   async execute(
     request: ISendNotificationRequest,
   ): Promise<ISendNotificationResponse> {
@@ -16,6 +19,8 @@ export class SendNotification {
       content: new Content(content),
       category,
     });
+
+    await this.notificationsRepository.create(notification);
 
     return {
       notification,
